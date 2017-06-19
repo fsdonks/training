8344f70c34ef624a12d6d96da671363286c17c40
joinr <vangelder@tantalus.net>
2017-06-19 16:02:52 -0500

    Simplified dependencies

--- project.clj
+++ project.clj
@@ -4,7 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [spork "0.1.9.7-SNAPSHOT"]
-                 [piccolotest  "0.1.0-SNAPSHOT"]
-                 [marathon "4.0.8-SNAPSHOT"]
                  [quil       "2.2.6"]])
